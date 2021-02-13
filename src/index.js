3bbe07950c0eba58399147601e2a8939c5c0c756
Seth Scoville <sscoville@roblox.com>
2021-02-13 16:37:49 -0600

    Finished Caching section

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import _ from "lodash";
-import printMe from './print.js';
+// import Print from './print';
 
 function component() {
-  const element = document.createElement("div");
-  const btn = document.createElement('button');
+    const element = document.createElement("div");
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(["Hello", "webpack"], " ");
+    // element.onclick = Print.bind(null, 'Hello webpack!');
 
-  element.appendChild(btn);
-
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: {
-        index: "./src/index.js",
-        print: "./src/print.js",
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-      },
+    entry: "./src/index.js",
     plugins: [
-        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Development',
+            title: "Caching",
         }),
     ],
     output: {
-        filename: "[name].bundle.js",
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: '/',
+    },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all",
+                },
+            },
+        },
     },
 };
