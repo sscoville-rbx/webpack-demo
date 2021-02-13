e83fc114105b3bdc36fa270e45c271116d070cb7
Seth Scoville <sscoville@roblox.com>
2021-02-13 15:42:08 -0600

    Finished Development section

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+        print: "./src/print.js",
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+      },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/',
+    },
 };
