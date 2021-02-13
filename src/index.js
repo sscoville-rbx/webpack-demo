30ac65aaa7a71c98e05165e7535defbd7e974170
Seth Scoville <sscoville@roblox.com>
2021-02-12 18:37:29 -0600

    Added image handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
+import MyAvatar from "./seth-avatar.jpg";
 
 function component() {
-    const element = document.createElement("div");
+  const element = document.createElement("div");
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
-    return element;
+  // Add the image to our existing div.
+  const myAvatar = new Image();
+  myAvatar.src = MyAvatar;
+
+  element.appendChild(myAvatar);
+
+  return element;
 }
 
 document.body.appendChild(component());
